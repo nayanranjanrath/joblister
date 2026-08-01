9b4c14ad211695e4d08f1c0bf8ad577c3a433fbe
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-08-01 09:52:24 +0530

    Add Express framework to the project and set up a basic server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import express from "express"
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
