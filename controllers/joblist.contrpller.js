08317015e7e57dd809fc2852307477e18d5d6627
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-08-07 23:36:13 +0530

    Add applicant rejection and requirements management features: implement reject applicant function with authorization checks; add requirements and skills to job model with unique constraints

--- controllers/joblist.contrpller.js
+++ controllers/joblist.contrpller.js
@@ -107,11 +107,100 @@ export const aproveapplicant = async (req, res) => {
     }
 }
 
-const addrequirements = async (req, res) => {
-    try {
+
+export const rejectapplicant = async (req, res) => {
+    try {  
+        const token = req.cookies.accesstoken;
+        if (!token) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+        const  applicationId  = req.body.applicationId ;
+        if (!applicationId) {
+            return res.status(400).json({ message: "Application ID is required" });
+        }
+        const userId = extractToken(token);
+        if (!userId) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+        const application = await prisma.application.findUnique({ where: { id: applicationId } });
+        if (!application) {
+            return res.status(404).json({ message: "Application not found" });
+        }
+        const job = await prisma.job.findUnique({ where: { id: application.jobId },
+        include: { postedby: true } });
+        if (!job) {
+            return res.status(404).json({ message: "Job not found" });
+        }
+        if (job.postedby.id !== userId) {
+            return res.status(403).json({ message: "You are not authorized to approve this applicant" });
+        }
+        const updatedApplication = await prisma.application.update({
+            where: { id: applicationId },
+            data: { status: "REJECTED" },
+        });
+        res.status(200).json({ message: "Applicant rejected successfully", updatedApplication });
+    }
         
+        
+     catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+export const addrequirements = async (req, res) => {
+    try {
+        const token = req.cookies.accesstoken;
+        if (!token) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+        const { jobId, expirience,description } = req.body;
+        if(!jobId){
+            return res.status(400).json({ message: "Job ID is required" });
+        }
+        const userId = extractToken(token);
+        if (!userId) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+        const job= await prisma.job.findUnique({ where: { id: jobId },include: { postedby: true } });
+        if (!job) {
+            return res.status(404).json({ message: "Job not found" });
+        }
+        if (job.postedby.id !== userId) {
+            return res.status(403).json({ message: "You are not authorized to add requirements to this job" });
+        }
+        const requirements = await prisma.requirements.create({
+            data: {
+                expirience,
+                description,
+                job: { connect: { id: jobId } },
+            },
+        });
+        res.status(201).json({ message: "Requirements added successfully", requirements });
     } catch (error) {
           console.log(error)
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const addskillrequired =async(req,res)=>{
+    try {
+        const token = req.cookies.accesstoken;
+        if (!token) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+        const { jobId, skill } = req.body;
+        if(!jobId || !skill){
+            return res.status(400).json({ message: "Job ID and skill are required" });
+        }
+        const userId = extractToken(token);
+        if (!userId) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+      const requirements = await prisma.requirements.findUnique({ where: { jobId } });
+      
+        res.status(201).json({ message: "Skill added successfully", skillrequired });
+    } catch (error) {
+        console.log(error)
         res.status(500).json({ message: "Internal server error" });
     }
 }
